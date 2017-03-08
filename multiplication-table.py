c7ad224cc3a09b01be57743e124ee6b144ad24db
Amovi <anthony.movizzo@outlook.com>
2017-03-08 13:48:59 -0500

    Updated from Brython Server: 3/8/2017 1:48:59 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -10,3 +10,7 @@ height = input (int("Multiplication table height"))
 [ 8   16  24  32   40   48   56   64   72   80]
 [ 9   18  24  32   45   54   63   72   81   90]
 [ 10  20  30  40   50   60   70   80   90   100]
+if width <= 
+
+"when width equals 'blank' print up to that place in the list, when height equals blank print the row"
+"something with ranges and looping for sure"
